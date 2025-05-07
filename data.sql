56b160f0dca3469643d3f93eb7d3190fa107cd87
Omangcool37 <jwesarnold409@Gmail.com>
2025-05-07 14:42:57 -0500

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,161 @@
+INSERT INTO format (format)
+VALUES ('physical');
+
+INSERT INTO format (format)
+VALUES
+('e-book'),
+('audio');
+
+
+
+INSERT INTO genre (genre)
+VALUES ('fantasy');
+
+INSERT INTO genre (genre)
+VALUES
+('superhero');
+INSERT INTO genre (genre)
+VALUES
+('mystery');
+INSERT INTO genre (genre)
+VALUES
+('psychological thriller');
+INSERT INTO genre (genre)
+VALUES
+('horror');
+INSERT INTO genre (genre)
+VALUES
+('children literature');
+INSERT INTO genre (genre)
+VALUES
+('comedy');
+INSERT INTO genre (genre)
+VALUES
+('action');
+INSERT INTO genre (genre)
+VALUES
+('dark fantasy');
+INSERT INTO genre (genre)
+VALUES
+('adventure');
+INSERT INTO genre (genre)
+VALUES
+('supernatural');
+INSERT INTO genre (genre)
+VALUES
+('shonen');
+INSERT INTO genre (genre)
+VALUES
+('historical');
+INSERT INTO genre (genre)
+VALUES
+('martial arts');
+
+INSERT INTO publisher (publisher)
+VALUES ('bloomsbury');
+
+
+INSERT INTO publisher (publisher)
+VALUES
+('dc comics'),
+('shueisha'),
+('scholastic'),
+('random house'),
+('skybound entertainment'),
+('amulet books'),
+('hakusensha'),
+('naver webtoon'),
+('vic medium'),
+('harper and brothers');
+('jeff kinney'),
+('kentaro miura'),
+('lee jong'),
+('takekiko iuninoue'),
+('geoff johns'),
+('e.b. white');
+INSERT INTO author (author)
+VALUES
+('brent bristol');
+
+INSERT INTO book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty,
+    cover_image
+)
+
+
+
+INSERT INTO book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('kingdom come',	2, 2, 1996, '1st edition', 1996, 'paperback', 5, 'english', 176, 10, 'superman.jpeg');
+INSERT INTO book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+
+values
+('percy jackson',	2, 2, 2005, '1st edition',2005, 'hardcover', 5, 'english', 377, 10, 'percyjackson.jpeg');
+INSERT INTO book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+values
+('lord of the rings',	2, 2, 1954, '1st edition', 1954, 'hardcover', 5, 'english', 1077, 10, 'lordoftherigns.jpeg');
+INSERT INTO book(
+    title,
+    author_id,
+    publisher_id,
+    copyright_year,
+    edition,
+    edition_year,
+    binding,
+    rating,
+    language,
+    num_pages,
+    qty, 
+    cover_image
+)
+
+
